d34e8402816d4ad43d32c1ba9cddd2a6101c8a36
[ayush tiwari] <[ayush0511tiwari@gmail.com]>
2023-12-17 16:43:00 +0530

    upload

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Hero from '@/components/Hero';
+
+export default function Home() {
+  return (
+    <main className='h-full w-full'>
+      <div className='flex flex-col h-[850px] gap-20'>
+        <Hero />
+        
+      </div>
+      
+    </main>
+    
+  )
+}
