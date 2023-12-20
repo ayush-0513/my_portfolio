db0c930a7b7a66075a366e833c792add978b5d15
[ayush tiwari] <[ayush0511tiwari@gmail.com]>
2023-12-20 08:23:24 +0530

    upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import StarsCanvas from '@/components/StarBackground'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,8 @@ export default function RootLayout({
       <body className={'${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden'}
       >
         <StarsCanvas />
+        <Navbar />
+        
         {children}
         </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Hero from '@/components/Hero';
 export default function Home() {
   return (
     <main className='h-full w-full'>
-      <div className='flex flex-col h-[850px] gap-20'>
+      <div className='flex flex-col gap-20'>
         <Hero />
         
       </div>
