8852d740935270c8285df932414b306be5cc4f79
[ayush tiwari] <[ayush0511tiwari@gmail.com]>
2023-12-21 20:41:17 +0530

    upload

---
+++ components/sub/SkillDataProvider.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React from 'react'
+import {motion} from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+
+interface Props {
+    src: string;
+    width: number;
+    height: number;
+    index: number;
+}
+
+const SkillDataProvider = ({ src, width, height, index} : Props) => {
+    const {ref, inView} = useInView({
+        triggerOnce: true
+    })
+
+    const imageVariants = {
+        hidden: {opacity: 0},
+        visible: {opacity:1}
+    }
+
+    const animationDelay = 0.3
+    
+  return (
+    <div>SkillDataProvider</div>
+  )
+}
+
+export default SkillDataProvider
