16d4f166e1ed2113ecb4b6f21e622963e747d25a
[ayush tiwari] <[ayush0511tiwari@gmail.com]>
2023-12-22 00:34:10 +0530

    upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import Hero from '@/components/Hero';
 import Skills from '@/components/Skills'
+import Encryption from '@/components/Encryption';
+import Projects from '@/components/Projects';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
@@ -8,6 +11,9 @@ export default function Home() {
       <div className='flex flex-col gap-20'>
         <Hero />
         <Skills />
+        <Encryption />
+        <Projects />
+        <Footer />
         
       </div>
       
